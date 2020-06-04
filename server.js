cb66bddd41fd0518271acaac36576e1cca8f9ffe
NattawatPisetsalasai <36739659+NattawatPisetsalasai@users.noreply.github.com>
2020-06-04 17:31:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.post('/', (req, res) => res.sendStatus(200)) {
+app.post('/', (req, res) => ) {
   res.json({
     message: 'OK'
   })
